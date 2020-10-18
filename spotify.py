fcc3f79f84b7f806c21f51f10df5cd01e0f212d5
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-10-18 16:49:52 +0200

    Playlist push and test

--- spotify.py
+++ spotify.py
@@ -12,7 +12,7 @@ class SpotifyClient(object):
 
     def fetch_user_playlists(self, username):
         playlists = {}
-        raw_playlists = self.spotipy_client.user_playlists(username)
+        raw_playlists = self.spotipy_client.user_playlists(username, limit=200)
         if raw_playlists and raw_playlists.get('items'):
             playlists = {
                 playlist['name'].lower(): playlist['uri']
